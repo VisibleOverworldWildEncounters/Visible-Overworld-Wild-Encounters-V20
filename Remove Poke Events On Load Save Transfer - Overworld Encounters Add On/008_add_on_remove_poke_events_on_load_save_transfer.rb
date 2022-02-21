ce9a1f921abb5e69b82ec243cc9c45a1103f533d
VisibleOverworldWildEncounters <100128151+VisibleOverworldWildEncounters@users.noreply.github.com>
2022-02-21 13:42:35 +0100

    Update 008_add_on_remove_poke_events_on_load_save_transfer.rb

--- Remove Poke Events On Load Save Transfer - Overworld Encounters Add On/008_add_on_remove_poke_events_on_load_save_transfer.rb
+++ Remove Poke Events On Load Save Transfer - Overworld Encounters Add On/008_add_on_remove_poke_events_on_load_save_transfer.rb
@@ -6,6 +6,17 @@
 
 #See  https://www.pokecommunity.com/showpost.php?p=10395097&postcount=382
 
+#During the implementations of new features I've been wondering about how to handle PokeEvents as temporals, so pokemon are not there after saving and loading up your game.
+#Since it is a hard task I came up with this Add-On that does something similar but in a forced manner.
+
+#This Add-On is a simple idea that removes all PokeEvents when loading a save and when transferring.
+
+#Concerning player transferring:
+#	- Sometimes a transfer to the same map can happen, which won't clear and update the map
+#	- If you want to use it on specific transfers you will have to delete the last part ("#Overide transfer_player"...) and call "pbRemovePokeEvents" right before transferring to another location of the same map.
+
+#Also, with this Add-On you can call the script "pbRemovePokeEvents" inside any event whenever needed.
+
 ##===============================================================================
 ## new methods to remove pkmn spawned 
 ##===============================================================================
