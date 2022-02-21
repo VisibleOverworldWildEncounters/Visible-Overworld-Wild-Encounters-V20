a672f2acb147700a6026cdc0f5b0551cc1e60950
VisibleOverworldWildEncounters <100128151+VisibleOverworldWildEncounters@users.noreply.github.com>
2022-02-21 15:48:26 +0100

    Update 001_visible overworld wild encounters script.rb

--- Visible Overworld Wild Encounters - Script/001_visible overworld wild encounters script.rb
+++ Visible Overworld Wild Encounters - Script/001_visible overworld wild encounters script.rb
@@ -20,6 +20,7 @@
 #===============================================================================
 # NEW FEATURES
 #  [*] Easy Install as Plugin
+#  [*] Set movement of overworld pokemon depending on its properties
 #  [*] set steps a pokemon remains on map before despawning depending on pokemon properties 
 #  [*] Choose whether you can battle water pokemon while not surfing or not
 #  [*] In water pokemon won't spawn above other tiles, which made them stuck or walk on ground
