e8751badb9311615169632ad2ce91e10e06d8a16
VisibleOverworldWildEncounters <100128151+VisibleOverworldWildEncounters@users.noreply.github.com>
2022-02-24 03:26:31 +0100

    Update 001_overworld_lavender_town_ghosts.rb

--- Overworld Lavender Town Ghosts/001_overworld_lavender_town_ghosts.rb
+++ Overworld Lavender Town Ghosts/001_overworld_lavender_town_ghosts.rb
@@ -11,7 +11,7 @@
 
 # INSTALLATION:
 # 1) Get or create an overworld sprite for your overworld ghost encounter. This plugin will not provide a sprite but maybe you find can easily find some resource, for example search for "shiny missingNo [Ghost Form]".
-# 2) Name that graphic "ghost.png" and place it in your "/Graphics/Characters/" folder of your project.
+# 2) Name that graphic "ghost.png" and place it in your "/Graphisc/Characters/" folder of your project.
 # 3) Download and install the "Lavender Town Ghosts" Plugin.
 # 4) Download and install the "Visible Overworld Wild Encounters" Plugin.
 # 5) Download and install this plugin.
@@ -19,14 +19,9 @@
 #-------------------------------------------------------------------------------
 # Overwriting method ow_sprite_filename to include overworld ghost sprite
 #-------------------------------------------------------------------------------
-module GameData
-    class Species
-        def self.ow_sprite_filename(species, form = 0, gender = 0, shiny = false, shadow = false)
-            return "Graphics/Characters/ghost.png" if Ghost.active?
-            ret = self.check_graphic_file("Graphics/Characters/", species, form, gender, shiny, shadow, "Followers")
-            ret = "Graphics/Characters/Followers/000" if nil_or_empty?(ret)
-            return ret
-        end
-    end
+alias ghost_ow_sprite_filename ow_sprite_filename
+def ow_sprite_filename(species, form = 0, gender = 0, shiny = false, shadow = false)
+    return "Graphics/Characters/ghost" if Ghost.active?
+    return ghost_ow_sprite_filename(species, form, gender, shiny, shadow)
 end
-  
+    
