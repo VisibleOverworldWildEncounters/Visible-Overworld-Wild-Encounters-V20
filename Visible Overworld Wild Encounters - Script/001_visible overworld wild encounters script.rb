df5603acf89151822daf0845c09e9b7fa147acd3
VisibleOverworldWildEncounters <100128151+VisibleOverworldWildEncounters@users.noreply.github.com>
2022-05-30 22:25:48 +0200

    Update 001_visible overworld wild encounters script.rb

--- Visible Overworld Wild Encounters - Script/001_visible overworld wild encounters script.rb
+++ Visible Overworld Wild Encounters - Script/001_visible overworld wild encounters script.rb
@@ -114,6 +114,7 @@
 # If you need help, found a bug or search for more modifications then go to
 # https://www.pokecommunity.com/showthread.php?t=429019
 
+
 #===============================================================================
 #             CHANGELOG
 #===============================================================================
@@ -202,18 +203,6 @@ module VisibleEncounterSettings
   # <= 0           - means only overworld encounters, no instant battles
   # > 0 and < 100  - means overworld encounters and normal encounters at the same time.
   # >= 100         - means only normal encounters and instant battles as usual, no overworld spawning
-  
-  NO_OF_CHOSEN_TILES = 1 # default 1
-  # Pokemon only spawn on allowed ground. This parameter needs to be an integer.
-  # It says up to how many different, random tiles in SPAWN_RANGE to the player
-  # will be checked for beeing allowed spawning ground.
-  # = 0  - means that it will choose a random tile until one of them 
-  #       is possible to spawn a pokemon or all tiles in SPAWN_RANGE to the player were chosen
-  #       (this will increase the spawn probability)
-  #       (may cause lag with other scripts)
-  # = 1  - means that only one random tile around the player will be chosen
-  # > 1   - maximal number of chosen tiles to find a possible tile where a pokemon can spawn
-  #         (the more you use, more performance needed)
   
   #--------------- SPAWN POSITION ------------------
   SPAWN_RANGE = 4 # default 4
@@ -406,42 +395,21 @@ def pbChooseTileOnStepTaken
   x = $game_player.x
   y = $game_player.y
   range = VisibleEncounterSettings::SPAWN_RANGE
-  rolls = VisibleEncounterSettings::NO_OF_CHOSEN_TILES # maybe 1 or higher or 0 (means rolling till there are all tiles checked near player)
-  rolls = 8*range*(range+1)/2 if rolls > 8*range*(range+1)/2 || rolls <= 0
-  positions = []
-  for j in 0..rolls-1 do
-    if j == 0
-      i = rand(range)
-      r = rand((i+1)*8)
-    else
-      k = rand(positions.length)
-      i,r = positions[k]
-    end
-    if r<=(i+1)*2
-      new_x = x-i-1+r
-      new_y = y-i-1
-    elsif r<=(i+1)*6-2
-      new_x = [x+i+1,x-i-1][r%2]
-      new_y = y-i+((r-1-(i+1)*2)/2).floor
-    else
-      new_x = x-i+r-(i+1)*6
-      new_y = y+i+1
-    end
-    return [new_x,new_y] if pbTileIsPossible(new_x,new_y)
-    return if j >= rolls-1
-    if j == 0
-      for i2 in 0..range-1
-        for r2 in 0..(i2+1)*8-1
-          positions.push([i2,r2])
-        end
-      end
-    end
-    positions.delete([i,r])
-    return if positions.length == 0
+  i = rand(range)
+  r = rand((i+1)*8)
+  if r<=(i+1)*2
+    new_x = x-i-1+r
+    new_y = y-i-1
+  elsif r<=(i+1)*6-2
+    new_x = [x+i+1,x-i-1][r%2]
+    new_y = y-i+((r-1-(i+1)*2)/2).floor
+  else
+    new_x = x-i+r-(i+1)*6
+    new_y = y+i+1
   end
+  return [new_x,new_y] if pbTileIsPossible(new_x,new_y)
   return
 end
-
 
 #===============================================================================
 # new method pbTileIsPossible to check if tile is good to spawn
