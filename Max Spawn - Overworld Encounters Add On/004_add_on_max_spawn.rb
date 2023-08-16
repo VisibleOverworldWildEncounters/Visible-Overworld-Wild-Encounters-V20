63ec26542686d5b93e8d0b6d82a82210db4b2f86
VisibleOverworldWildEncounters <100128151+VisibleOverworldWildEncounters@users.noreply.github.com>
2023-08-16 22:39:09 +0200

    Update 004_add_on_max_spawn.rb

--- Max Spawn - Overworld Encounters Add On/004_add_on_max_spawn.rb
+++ Max Spawn - Overworld Encounters Add On/004_add_on_max_spawn.rb
@@ -2,22 +2,30 @@
           #########################################################
           #                                                       #
           #          ADD-ON:  MAX SPAWN by TrankerGolD            #
-          #                                                       #
+          #                              -updated by derFischae   #
           #########################################################
 
 # You can use this add-on to set a maximal number of wild Encounters Events
 # that can be spawned on the map at the same time.
-# Use the parameter MAX_SPAWN (see below) to set the limit of overworld pokeEvents at the same time.
+# Use the parameter DEFAULT_MAX_SPAWN (see below) to set the limit of overworld pokeEvents at the same time.
+# Use the parameter MAP_MAX_SPAWN (see below) to set specific limits for each map differently. 
 
 # FEATURES:
-# * Stop more PokeEvent from spawning with the MAX_SPAWN parameter
+# * Stop more PokeEvent from spawning with the DEFAULT_MAX_SPAWN parameter and give exceptions for each map in MAP_MAX_SPAWN.
 
 
 module VisibleEncounterSettings
-  MAX_SPAWN = 0 # default 0
-  # MAX_SPAWN is the max number of wild Encounters Events that can be spawned on the map at the same time.
+  DEFAULT_MAX_SPAWN = 0 # default 0
+  # DEFAULT_MAX_SPAWN is the max number of wild Encounters Events that can be spawned on the map at the same time.
   # <=0  - means infinite (no maximum)
-  # >0   - maximum of wild encounters on the map  
+  # >0   - equals maximum of wild encounters on all maps (that are not listed as exceptions in MAP_MAX_SPAWN below) 
+
+  MAP_MAX_SPAWN = [
+    [4, 5]              # - means the maximal spawn limit is 5 on map with map_id 4
+    #[map_id, limit],   # - add your specific map limits here. Don't forget to use a comma between your pairs.
+  ]
+  # MAP_MAX_SPAWN is a parameter that contains pairs of the form [map_id, limit], where each pair consists of the map_id of a map and the corresponding spawn limit of that map.
+  # example: [ [4,5], [6,0] ]   means   "map with id 4 has spawn limit of 5 pokemons and map with id 6 has no spawn limit".
 end
 
 #===============================================================================
@@ -26,7 +34,27 @@ end
 #===============================================================================
 alias o_pbSpawnOnStepTaken pbSpawnOnStepTaken
 def pbSpawnOnStepTaken(repel_active)
-  return false if VisibleEncounterSettings::MAX_SPAWN>0 && pbCountPokeEvent >= VisibleEncounterSettings::MAX_SPAWN
+  max_spawn = VisibleEncounterSettings::DEFAULT_MAX_SPAWN
+
+  currentRegion = pbGetCurrentRegion
+  currentMapName = $game_map.name
+  if MAP_MAX_SPAWN && MAP_MAX_SPAWN.length > 0 do
+    for map_limit in MAP_MAX_SPAWN do
+      map_id = map_limit[0]
+
+      if map_id != $game_map.map_id
+        map_metadata = GameData::MapMetadata.try_get(map_id)
+        next if !map_metadata || !map_metadata.town_map_position ||
+                map_metadata.town_map_position[0] != currentRegion
+        next if pbGetMapNameFromId(map_id) != currentMapName
+      end
+      
+      max_spawn = map_limit[1]
+      break
+    
+    end
+  end
+  return false if max_spawn>0 && pbCountPokeEvent >= max_spawn
   o_pbSpawnOnStepTaken(repel_active)
 end 
 
@@ -64,4 +92,6 @@ def pbCountPokeEventInMap
   }
   return currentCountPokeEvent
 end
-  
+
+
+
