98efcdebf8540effa141052d350307b37c548d84
VisibleOverworldWildEncounters <100128151+VisibleOverworldWildEncounters@users.noreply.github.com>
2023-12-16 11:23:30 +0100

    Update 001_visible overworld wild encounters script.rb

--- Visible Overworld Wild Encounters - Script/001_visible overworld wild encounters script.rb
+++ Visible Overworld Wild Encounters - Script/001_visible overworld wild encounters script.rb
@@ -7,7 +7,7 @@
 
 # This script is for Pokémon Essentials v20 (for short PEv20). UPDATED TO VERSION 20.0.0.4.
 
-# As in Pokemon Let's go Pikachu/Eevee or Pokemon Shild and Sword wild encounters
+# As in Pokemon Let's go Pikachu/Eevee or Pokemon Shield and Sword wild encounters
 # pop up on the overworld, they move around and you can start the battle with
 # them simply by moving to the pokemon. Clearly, you also can omit the battle
 # by circling around them.
